99985df6c4c27e22175ee4db9ca6b86abb284581
doug-pires <dpiresmartins@yahoo.com.br>
2023-06-04 15:23:29 +0100

    feat[transform.utils]:Test transform.utils functions to save as Delta table

--- fipe/elt/load/utils.py
+++ fipe/elt/load/utils.py
@@ -31,10 +31,19 @@ def run_vacumm():
 
 
 if __name__ == "__main__":
-    path_development = Path().cwd()
+    additional_options = {
+        "spark.master": "local[1]",
+        # Add other additional options here
+    }
+    spark_manager = SparkSessionManager(
+        app_name=__name__, additional_options=additional_options
+    )
+    spark = spark_manager.get_spark_session()
+    spark_manager.print_config()
 
-
+    # Path Development
+    path_development = Path().cwd().as_posix()
     schema = StructType([StructField("reference_month", StringType(), False)])
     df = transform_to_df(spark, data=["maio/2023", "abril/2023"], schema=schema)
-    save_as_delta(df=df, path=path_development.as_posix())
+    save_as_delta(df=df, path=path_development)
     df.show()
