256b09411574000b6f1a48fa2093a9580368ea4c
doug-pires <dpiresmartins@yahoo.com.br>
2023-07-11 23:59:46 +0100

    feat: Add docstrings for all automation functions

--- fipe/pipeline/extract_load_tables.py
+++ fipe/pipeline/extract_load_tables.py
@@ -54,14 +54,13 @@ def main():
     # Start Workflow
     ################
     start_time = time.time()
-    for month_year in list_reference_month_year[:1]:
-        time.sleep(1)
+    for month_year in list_reference_month_year[:2]:
+        time.sleep(0.5)
         bt_month_year = locate_bt(
             driver=site_fipe,
             xpath=xpath_bt_month_year,
         )
         click(bt_month_year)
-        time.sleep(0.5)
 
         # Define List will keep the dictionaries of Data
         list_of_dicts = []
@@ -70,8 +69,8 @@ def main():
         # For Each Reference Month extract all Brands Available
         ################
         list_brands = scrape_options_brands(site_fipe)
-        logger.info(f"Brands available: {list_brands[:8]} for {month_year}")
-        for brand in ["BMW"]:
+
+        for brand in list_brands:
             time.sleep(0.5)
             bt_brand = locate_bt(site_fipe, xpath_bt_brand)
             add_on(bt_brand, brand)
@@ -81,18 +80,18 @@ def main():
             list_models = scrape_options_models(site_fipe)
 
             for model in list_models:
+                scroll_to_element(driver=site_fipe, xpath=xpath_bt_model)
                 bt_model = locate_bt(driver=site_fipe, xpath=xpath_bt_model)
                 click(bt_model)
-                time.sleep(3)
                 add_on(bt_model, model)
+
                 # For Each Model extract all Manufacturing Year - Fuel Available
                 ################
                 list_manufacturing_year_fuel = scrape_manufacturing_year_fuel(site_fipe)
                 logger.info(
-                    f"Manufacturing Year available: {list_manufacturing_year_fuel[:8]} for {month_year} and brand {brand}, model: {model}"
+                    f"Manufacturing Year available: {list_manufacturing_year_fuel} for {month_year} and brand {brand}, model: {model}"
                 )
                 for manufacturing_year in list_manufacturing_year_fuel:
-                    time.sleep(1)
                     bt_manufacturing_year = locate_bt(
                         site_fipe, xpath_bt_manufacturing_year_fuel
                     )
@@ -100,37 +99,43 @@ def main():
                     add_on(bt_manufacturing_year, manufacturing_year)
 
                     # Press Search
+                    # Sometimes this will be searching for the Table, then we will try to skip if fails
                     bt_search = locate_bt(site_fipe, xpath_bt_search)
                     click(bt_search)
+                    time.sleep(0.5)
 
                     # Extract All Table
-
                     data = scrape_complete_tbody(site_fipe, new_columns_df_bronze)
 
                     list_of_dicts.append(data)
-                print("Finished Year Fuel, picking another Model")
-
-                time.sleep(3)
 
                 # Clean Search
+                # We are cleaning because some model are not available for the SPECIFIC Manufacturing Year - Fuel left by the last process.
+                # The options were REFRESH the PAGE OR Clean the fields EXCEPT the reference_month.
+                # That's why I decided clean the table and add the BRAND again.
                 bt_clean = locate_bt(site_fipe, xpath_bt_clean_search)
                 click(bt_clean)
-            df = transform_to_df(spark, list_of_dicts, schema_df_fipe_bronze)
-            print("Brand: ", brand)
-            save_delta_table_partitioned(
-                df=df,
-                path=path_dev,
-                mode="append",
-                delta_table_name="fipe_bronze",
-                partition_by=["reference_month", "brand"],
-            )
+                time.sleep(0.5)
+
+                # After Clean, lWe will add again the Brand.
+                click(bt_brand)
+                add_on(bt_brand, brand)
+
+        df = transform_to_df(spark, list_of_dicts, schema_df_fipe_bronze)
+        save_delta_table_partitioned(
+            df=df,
+            path=path_dev,
+            mode="append",
+            delta_table_name="fipe_bronze",
+            partition_by=["reference_month", "brand", "model"],
+        )
 
     list_of_dicts.clear()
 
     close_browser(site_fipe)
     end_time = time.time()
     total_time = end_time - start_time
-    print(round(total_time))
+    print(f"Took {round(total_time,2)} seconds")
 
 
 if __name__ == "__main__":
--- fipe/scripts/utils.py
+++ fipe/scripts/utils.py
@@ -16,6 +16,15 @@ logger = get_logger(__name__)
 
 
 def open_chrome(url: str, headless: bool = True):
+    """Open Google Chrome browser and navigate to the specified URL.
+
+    Args:
+        url (str): The URL to navigate to.
+        headless (bool, optional): Whether to run the browser in headless mode. Defaults to True.
+
+    Returns:
+        webdriver.Chrome: The Chrome WebDriver instance.
+    """
     options = webdriver.ChromeOptions()
     if headless:
         options.add_argument("--headless")
@@ -40,16 +49,42 @@ def open_chrome(url: str, headless: bool = True):
 
 
 def close_browser(driver: WebDriver):
+    """Close the browser.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver instance.
+
+    Returns:
+        None
+    """
     return driver.close()
 
 
 # General Functions
 def locate_bt(driver: WebDriver, xpath: str) -> WebElement:
+    """Locate a button element on the page using XPath.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver instance.
+        xpath (str): The XPath expression to locate the button element.
+
+    Returns:
+        WebElement: The located button element.
+    """
     bt_general = driver.find_element(By.XPATH, xpath)
     return bt_general
 
 
 def scroll_to_element(driver: WebDriver, xpath: str) -> WebElement | None:
+    """Scroll to an element on the page using JavaScript.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver instance.
+        xpath (str): The XPath expression to locate the element.
+
+    Returns:
+        WebElement or None: The located element after scrolling or None if the element is not found.
+    """
     js_code = "arguments[0].scrollIntoView();"
     try:
         bt = driver.find_element(By.XPATH, xpath)
@@ -59,15 +94,41 @@ def scroll_to_element(driver: WebDriver, xpath: str) -> WebElement | None:
 
 
 def click_with_driver(driver: WebDriver, bt_or_box: str):
+    """Click on an element using the Selenium WebDriver.
+
+    Args:
+        driver (WebDriver): The Selenium WebDriver instance.
+        bt_or_box (str): The element to click, identified by a CSS selector, XPath expression, or other locator.
+
+    Returns:
+        None
+    """
     js_code = "arguments[0].click();"
     return driver.execute_script(js_code, bt_or_box)
 
 
 def click(bt_or_box):
+    """Click on a WebElement.
+
+    Args:
+        bt_or_box (WebElement): The WebElement to click.
+
+    Returns:
+        None
+    """
     return bt_or_box.click()
 
 
 def add_on(bt_or_box, info: str):
+    """Add information to an element and press Enter.
+
+    Args:
+        bt_or_box (WebElement): The WebElement to interact with.
+        info (str): The information to be added.
+
+    Returns:
+        None
+    """
     bt_or_box.send_keys(info)
     return bt_or_box.send_keys(Keys.ENTER)
 
