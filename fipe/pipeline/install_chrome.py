824450db3f04345c74c795a0e1ba9dee8d3622d8
doug-pires <dpiresmartins@yahoo.com.br>
2023-09-17 01:42:58 +0100

    feat: Add script to install chrome ( finalized ) and renamed script to extract average prices on demand

--- fipe/pipeline/install_chrome.py
+++ fipe/pipeline/install_chrome.py
@@ -2,3 +2,13 @@
 
 # MAGIC %sh
 # MAGIC sudo apt update && sudo apt upgrade -y --fix-missing > /dev/null 2>&1 &
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC wget  -P /usr/ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1 &
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo apt install /usr/google-chrome-stable_current_amd64.deb --fix-broken --fix-missing --yes
